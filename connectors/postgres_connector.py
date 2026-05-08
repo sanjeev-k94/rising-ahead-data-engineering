9d300963b9ec7931e36158e865a0dfc394740e58
sanjeev-k94 <76481859+sanjeev-k94@users.noreply.github.com>
2026-05-08 16:45:39 +0530

    Added GitHub Actions CI/CD pipeline

--- connectors/postgres_connector.py
+++ connectors/postgres_connector.py
@@ -1,5 +1,18 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/data_platform"
+from pipeline.config.load_config import load_config
+
+
+config = load_config()
+
+db = config["postgres"]
+
+DATABASE_URL = (
+    f"postgresql://{db['user']}:"
+    f"{db['password']}@"
+    f"{db['host']}:"
+    f"{db['port']}/"
+    f"{db['database']}"
+)
 
 engine = create_engine(DATABASE_URL)
--- pipeline/assets/silver.py
+++ pipeline/assets/silver.py
@@ -1,10 +1,14 @@
-from dagster import asset
+from dagster import asset, get_dagster_logger
 import pandas as pd
 from connectors.postgres_connector import engine
+import hashlib
 
-from dagster import asset
-import pandas as pd
-from connectors.postgres_connector import engine
+
+def generate_hash(row):
+
+    return hashlib.md5(
+        str(row.values).encode()
+    ).hexdigest()
 
 
 def clean_dataframe(df):
@@ -15,84 +19,9 @@ def clean_dataframe(df):
 
     df = df.fillna("unknown")
 
-    return df
-
-
-@asset
-def silver_customers():
-
-    df = pd.read_sql(
-        "SELECT * FROM bronze_customers",
-        engine
-    )
-
-    df = clean_dataframe(df)
-
-    df.to_sql(
-        "silver_customers",
-        engine,
-        if_exists="replace",
-        index=False
-    )
-
-    return df
-
-
-@asset
-def silver_accounts():
-
-    df = pd.read_sql(
-        "SELECT * FROM bronze_accounts",
-        engine
-    )
-
-    df = clean_dataframe(df)
-
-    df.to_sql(
-        "silver_accounts",
-        engine,
-        if_exists="replace",
-        index=False
-    )
-
-    return df
-
-
-@asset
-def silver_transactions():
-
-    df = pd.read_sql(
-        "SELECT * FROM bronze_transactions",
-        engine
-    )
-
-    df = clean_dataframe(df)
-
-    df.to_sql(
-        "silver_transactions",
-        engine,
-        if_exists="replace",
-        index=False
-    )
-
-    return df
-
-
-@asset
-def silver_loans():
-
-    df = pd.read_sql(
-        "SELECT * FROM bronze_loans",
-        engine
-    )
-
-    df = clean_dataframe(df)
-
-    df.to_sql(
-        "silver_loans",
-        engine,
-        if_exists="replace",
-        index=False
+    df["row_hash"] = df.apply(
+        generate_hash,
+        axis=1
     )
 
     return df
